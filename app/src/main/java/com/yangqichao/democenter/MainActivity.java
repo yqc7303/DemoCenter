880991bd57c759401c13372a6967366fd097e937
杨旗超 <793477679@qq.com>
2017-05-22 17:05:47 +0800

    集成epub

--- app/src/main/java/com/yangqichao/democenter/MainActivity.java
+++ app/src/main/java/com/yangqichao/democenter/MainActivity.java
@@ -1,24 +1,31 @@
 package com.yangqichao.democenter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.chad.library.adapter.base.listener.OnItemClickListener;
+import com.folioreader.activity.FolioActivity;
+import com.yangqichao.democenter.business.bean.SampleBean;
 import com.yangqichao.democenter.common.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
-import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
+    @BindView(R.id.recycler_demo)
+    RecyclerView recyclerDemo;
+    private BaseQuickAdapter<SampleBean, BaseViewHolder> adapter;
+    private List<SampleBean> sampleBeanList;
 
-    @BindView(R.id.vfvfvvf)
-    TextView vfvfvvf;
-    @BindView(R.id.button)
-    Button button;
-    @BindView(R.id.button2)
-    Button button2;
+
 
     @Override
     protected int getLayoutResID() {
@@ -30,18 +37,37 @@ public class MainActivity extends BaseActivity {
         showToast("DEBUG:" + BuildConfig.DEBUG + "\n BUILD_TYPE:" + BuildConfig.BUILD_TYPE + "\n VERSION_CODE:" + BuildConfig.VERSION_CODE);
         showToast(BuildConfig.BaseUrl);
 
+        sampleBeanList = new ArrayList<>();
+        sampleBeanList.add(new SampleBean("ePud阅读器", FolioActivity.class));
+
+        adapter = new BaseQuickAdapter<SampleBean, BaseViewHolder>(R.layout.item_demo,sampleBeanList) {
+            @Override
+            protected void convert(BaseViewHolder helper, SampleBean item) {
+                helper.setText(R.id.sample_name,item.getName());
+            }
+        };
+
+        recyclerDemo.setLayoutManager(new LinearLayoutManager(this));
+        recyclerDemo.setAdapter(adapter);
+        recyclerDemo.addOnItemTouchListener(new OnItemClickListener() {
+            @Override
+            public void onSimpleItemClick(BaseQuickAdapter adapter, View view, int position) {
+
+                Class aClass = sampleBeanList.get(position).getActivity();
+                Intent intent = new Intent(MainActivity.this, aClass);
+                if(aClass == FolioActivity.class){
+                    intent.putExtra(FolioActivity.INTENT_EPUB_SOURCE_TYPE,FolioActivity.EpubSourceType.ASSESTS);
+                    intent.putExtra(FolioActivity.INTENT_EPUB_SOURCE_PATH,"varun.epub");
+                }
+                startActivity(intent);
+            }
+        });
+
     }
 
 
-    @OnClick({R.id.button, R.id.button2})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button:
-                showToast("1111111");
-                break;
-            case R.id.button2:
-                showToast("2222222");
-                break;
-        }
-    }
+
+
+
+
 }
---
+++ app/src/main/java/com/yangqichao/democenter/business/ui/EpubReaderActivity.java
@@ -0,0 +1,15 @@
+package com.yangqichao.democenter.business.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yangqichao.democenter.R;
+
+public class EpubReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_epub_reader);
+    }
+}
